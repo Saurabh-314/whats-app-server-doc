d67ba2cf92243bffa42f8e4f38e1d82b3a41b723
Saurabh-314 <maxxxjob314@gmail.com>
2023-06-18 12:23:24 +0530

    change db url

--- database/db.js
+++ database/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const Connection = async () => {
-  const URL = "mongodb://127.0.0.1:27017/whats-app-doc";
+  // const URL = "mongodb://127.0.0.1:27017/whats-app-doc";
+  const URL = `mongodb+srv://saurabh-314:saurabh123@cluster0.jfrinjg.mongodb.net/whats-app-doc?retryWrites=true&w=majority`
   try {
     await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
     console.log("database connection successfull");
