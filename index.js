6eaaf7ec048b564826c1a044833ab99dafc3b48f
Saurabh-314 <maxxxjob314@gmail.com>
2023-06-18 09:20:55 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+
+app.use(cors());
+
+const io = new Server(server);
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("hell its working...");
+}) 
+
+io.on('connection', (socket) => {
+  console.log("a user connected");
+  socket.on('disconnect', () => {
+    console.log("user disconnected");
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+})
